46a75548b90b84d3a9274a7b12bcf919b10ffc53
Nabadeep <nabadeepdas069@gmail.com>
2025-02-11 22:18:29 +0530

    srt

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContactForm from '../components/ContactForm'
+import NavBar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const ContactPage = () => {
+  return (
+    <>
+    <NavBar/>
+    <ContactForm/>
+    <Footer/>
+    </>
+  )
+}
+
+export default ContactPage
